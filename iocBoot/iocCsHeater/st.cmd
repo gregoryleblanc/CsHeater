214d14ed68d0dccb227f382356e1fc8a19c116fa
Gregory Leblanc <headmaster.albus.dumbledore+github@gmail.com>
2021-08-10 17:13:37 -0400

    Add links to documentation and documented some lines of code.

--- iocBoot/iocCsHeater/st.cmd
+++ iocBoot/iocCsHeater/st.cmd
@@ -23,26 +23,64 @@ iocInit
 #====================================================================
 # modbus support
 
-#drvModbusAsynConfigure(portName, tcpPortName, slaveAddress, modbusFuction, modbusStartAddress, modbusLength, dataType, pollMsec, plcType)
-##  modbusFunction,  these are 16 bit registers.  Use 4 for read-only registers, 3 for r/w registers or 6 for write registers.
+#drvModbusAsynConfigure(portName, tcpPortName, slaveAddress, modbusFuction, modbusStartAddress, modbusLength, dataType,
+#                       pollMsec, plcType)
+##  modbusFunction,  these are 16 bit registers.  Use 4 for read-only registers, 3 for r/w registers or 6 for write
+##  registers.
 ##  modbusStartAddress = Use the realtive address in place of the full absolute address.
-##  modbusLength,  see spreadsheet for the various menus.  May require multiple configuration definintions per menu due to the limitation of the length definitions.
-##  dataType,  there are multiple data types within a menu so it's presumed these will need to be segregated which will expand the number of configurations.
+##  modbusLength,  see spreadsheet for the various menus.  May require multiple configuration definintions per menu
+##  due to the limitation of the length definitions.
+##  dataType,  there are multiple data types within a menu so it's presumed these will need to be segregated which
+##  will expand the number of configurations.
 ##             testing different options to deteremine what will work most efficiently.
 
+### set up the TCP/IP comms for modbus
 drvAsynIPPortConfigure("watlow1", "10.0.0.196", 0, 0, 0)
+##  https://epics-modbus.readthedocs.io/en/latest/#tcp-ip
+##  drvAsynIPPortConfigure("portName","hostInfo",priority,noAutoConnect,noProcessEos)
+
+### Add the link-specific bits for ModbusTCP
 modbusInterposeConfig("watlow1", 0, 2000, 0)
+##  https://epics-modbus.readthedocs.io/en/latest/#modbusinterposeconfig
+##  modbusInterposeConfig(portName,linkType,timeoutMsec,writeDelayMsec)
+
+### create the modbus port driver
+### 16 == modbus write multiple registers
 drvModbusAsynConfigure("watlow.w.1", "watlow1", 0, 16, -1, 4, 0, 2000, "watlow1");
+##  https://epics-modbus.readthedocs.io/en/latest/#drvmodbusasynconfigure
+##  drvModbusAsynConfigure(portName,tcpPortName,slaveAddress,modbusFunction,modbusStartAddress,modbusLength,
+##                         dataType,pollMsec,plcType);
+
+### create the modbus port driver
+### 6 == modbus write single register
 drvModbusAsynConfigure("watlow.enum.w.1", "watlow1", 0, 6, -1, 2, 0, 2000, "watlow1");
+
+
+
 
 ##  These configs are to be used for read or input records
 
 ##  Port Naming Convention:  Watlow_MD_DT_AI.I   where
 ##                              MD = Menu Description
+##                                  AI == Analog Input
+##                                  AO == Analog Output
+##                                  Al == Alarm
+##                                  Glb == FIXME
+##                                  Lim == FIXME
+##                                  Lin == FIXME
+##                                  CL == FIXME
+##                                  AO2 == FIXME
+##                                  AO3 == FIXME
+##                                  Prc == FIXME
 ##                              DT = Data Type
-##                              AI = Address Index (for the purpose when the range of addresses is greater the maximum allowable.  Using blocks of 100 to make indexing simpler)
+##                                  Num == Number (float? int? double?)
+##                                  Enum == I think from here:
+##                                          https://github.com/epicsdeb/modbus/blob/61e61608e758c457b3619c0c71523f8b12f1b11c/modbusApp/src/drvModbusAsyn.c#L75
+##                              AI = Address Index (for the purpose when the range of addresses is greater the maximum
+##                                   allowable.  Using blocks of 100 to make indexing simpler)
 ##								 I = Module Index
 
+###  4 == modbus read input registers (16-bit registers)
 drvModbusAsynConfigure("watlow_Glb_Num_1.1",  "watlow1", 0, 4,    0, 16, 7, 2000, "watlow1");
 drvModbusAsynConfigure("watlow_Glb_Enum_1.1", "watlow1", 0, 4, 2308,  4, 6, 2000, "watlow1");
 
